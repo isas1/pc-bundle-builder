1c4eeadb92404c6f2d4f7977b87529934a6817ed
isas1 <isas1_15@uni.worc.ac.uk>
2016-02-27 20:47:31 +0000

    background to blue

--- mobile application test/ViewController.swift
+++ mobile application test/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.view.backgroundColor = UIColor.blueColor();
+        self.view.backgroundColor = UIColor.blueColor();
         // Do any additional setup after loading the view, typically from a nib.
     }
 
